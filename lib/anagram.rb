9f73b2c02f35df39f3fca77c1f9960b13f78cbfa
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-08 04:31:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    comb = @word.split("").permutation.map &:join
+    match = array & comb
+    match
+  end
+end
